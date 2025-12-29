df4bbadfbe377efd2680c882a456e2e7398a5039
mehulvarshney101 <152776859+mehulvarshney101@users.noreply.github.com>
2025-12-29 12:47:23 +0000

    ok

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 64
+let accountEmail = "mehul@gmail.com"
+var accountPassword = "12345"
+accountCity = 'jaipur' //it is a implicit global;
+let accountState //It is undefined
+
+// accountId = 2
+accountEmail = "mehul1@gmail.com"
+accountPassword = "23456"
+accountCity = "Bengluru"
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict" //treat all JS code as newer version
+
+// alert("hello") // as we are using node js note browser
+
+console.log(3+
+    3) //code readabilty should be high
+
+
